dcaa03f437e41b7feae8b9e31468bcb304703cb1
Eliasmal Rodriguez <Eliasmal0228@gmail.com>
2025-04-09 13:59:30 +0000

    insertando imagenes

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,10 +7,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
     let array = [
-		{title:"1" , text:"1", img:""},
-		{title:"2" , text:"2", img:""},
-		{title:"3" , text:"3", img:""},
-		{title:"4" , text:"4", img:""},
+		{title:"Paisajes" , text:"Conoce los increibles paisajes de nuestro mundo", img:"https://wallpapers.com/images/featured/fondos-de-hermosos-paisajes-wnxju2647uqrcccv.jpg"},
+		{title:"Playas" , text:"Las mas hermosas playas del caribe", img:"https://img.freepik.com/fotos-premium/exoticos-paisajes-playa-tropical-suenos_287743-1067.jpg"},
+		{title:"Comida" , text:"Los platos mas valorados mundialmente", img:"https://editorialtelevisa.brightspotcdn.com/58/eb/b537a4714c11890dfba66649ee13/flautas-transformed.jpeg"},
+		{title:"Animales" , text:"Animales que no creeras que existen", img:"https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2018/02/21/239349.jpg"},
 	];
 
 	
--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -5,9 +5,9 @@ const jumbotron = () => {
 
         <div className="p-3 mb-4 ">
             <div className="container-fluid py-5 bg-body-tertiary rounded-3 ">
-                <h1 className="display-5 fw-bold">A Warm Welcome</h1>
+                <h1 className="display-5 fw-bold">Curiosidades por el mundo</h1>
                 <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cupiditate nemo quaerat excepturi suscipit voluptate, ratione expedita harum cum. Tempora quas dignissimos magni rerum qui harum nesciunt consectetur voluptate nihil!</p>
-                <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
+                <button className="btn btn-primary btn-lg" type="button">Enterate ya!</button>
             </div>
         </div>
     );

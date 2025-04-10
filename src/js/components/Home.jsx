6cebef62f2de0c5b9a645d50da337ef50f92fa6c
Eliasmal Rodriguez <Eliasmal0228@gmail.com>
2025-04-10 12:20:05 +0000

    cambiar nombre a array

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Card from "./Card";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-    let array = [
+    let myCards = [
 		{title:"Paisajes" , text:"Conoce los increibles paisajes de nuestro mundo", img:"https://wallpapers.com/images/featured/fondos-de-hermosos-paisajes-wnxju2647uqrcccv.jpg"},
 		{title:"Playas" , text:"Las mas hermosas playas del caribe", img:"https://img.freepik.com/fotos-premium/exoticos-paisajes-playa-tropical-suenos_287743-1067.jpg"},
 		{title:"Comida" , text:"Los platos mas valorados mundialmente", img:"https://editorialtelevisa.brightspotcdn.com/58/eb/b537a4714c11890dfba66649ee13/flautas-transformed.jpeg"},
@@ -17,7 +14,7 @@ const Home = () => {
 	return (
 		<div className=" px-3 d-flex justify-content-between text-center">
 			{
-				array.map((item,index) =>(
+				myCards.map((item,index) =>(
 					<Card key={index} title={item.title} text={item.text} img={item.img}/>
 				))
 			}
